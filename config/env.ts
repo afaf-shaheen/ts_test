66e1b1e0ef8925d4dbec3623156f4aeed9d5b5f3
afaf <afoof1233sh@gmail.com>
2026-08-18 16:54:59 +0300

    fix

--- config/env.ts
+++ config/env.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
+import type { SignOptions } from "jsonwebtoken";
+
 if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is missing from environment variables");
 }
 
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1h";
+export const JWT_EXPIRES_IN: SignOptions["expiresIn"] = 
+    (process.env.JWT_EXPIRES_IN as SignOptions["expiresIn"]) || "1h";
